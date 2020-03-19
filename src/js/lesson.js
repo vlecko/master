16e48345d6e64adc8c0b4a9a624581ae50dd0ca4
vlecko <52931749+vlecko@users.noreply.github.com>
2020-03-19 19:06:34 +0300

    dirik

--- src/js/lesson.js
+++ src/js/lesson.js
@@ -1,1 +1,48 @@
-console.log("ghfgh")
+    const testConst ='34';
+
+    console.log(testConst);
+    const x=7
+        if (x<7){
+            console.log('1')
+        }
+        else{
+            console.log('2');
+        if(x==7)
+            console.log('3')
+        }
+        let instr = 2
+
+    switch (instr) {
+        case 2:
+            console.log('tandem');
+            break;
+
+        case 1:
+            console.log('single');
+            break;
+    
+        default:
+            console.log('ffa');
+            break;
+    }
+    const number = +2
+    if (number ===0){
+        console.log('f');
+        alert('вы вывели число 1');
+    }
+    if (number ===1){
+        console.log('ff');
+        alert('вы вывели число 1');
+    }
+    if (number ===2 ){
+        console.log('ffa');
+        alert('вы вывели число 1');
+    }
+    function showMessage() {
+        alert( 'Всем привет!' );
+      }
+      
+      showMessage();
+      showMessage();
+
+    
