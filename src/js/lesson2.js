715294e9a6f9747f0449b1e39e05a025d0c15cc2
vlecko <52931749+vlecko@users.noreply.github.com>
2020-03-23 17:42:49 +0300

    10 x

--- src/js/lesson2.js
+++ src/js/lesson2.js
@@ -1,20 +1,10 @@
 // for (let index = 1; index = 1; index=index+0) {
-//     console.log(index=)  
+//     console.log(index=-0)  
 // }
 // console.log("index")
-
-// let counter = 0 
-// do {
-//     console.log(counter)
-//     counter=counter-1;
-    
-// } while (counter >=1);
-
-
+  
+    for (let i = 1; i <= 10; i=i+1) {
+        console.log('x');
+    }
  
-for (let i = 1; i <= 100; i++) {
-   if (i % 2 == 0) {
-     console.log( i );
-   }
-}
 
