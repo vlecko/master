5232e98922e3f1069b35edccacd5508305066a9c
vlecko <52931749+vlecko@users.noreply.github.com>
2020-04-28 22:14:53 +0300

    lesson

--- src/js/.js/array3.js
+++ src/js/.js/array3.js
@@ -156,3 +156,27 @@
 // }
 // const number = 10;
 // console.log(haesa(number));
+
+// function naesa(num) {
+//     const resalt = {
+//       nass: [],
+//       max: num,
+//     };
+//     while (num !== 1) {
+//       if (resalt.max < num) {
+//         resalt.max = num;
+//       }
+//       if (num % 2 === 0) {
+//         num = num / 2;
+//       } else {
+//         num = num * 3 + 1;
+//       }
+//       resalt.nass.push(num);
+//     }
+//     resalt.steps = resalt.nass.length;
+//     return resalt;
+//   }
+//   const arr = 9;
+//   const x = naesa(arr);
+//   console.log(x);
+//   console.log(x.steps);
--- src/js/lesson4.js
+++ src/js/lesson4.js
@@ -1,13 +1,23 @@
-function naesa(p) {
-  let num = p;
+function naesa(num) {
+  const resalt = {
+    nass: [],
+    max: num,
+  };
   while (num !== 1) {
+    if (resalt.max < num) {
+      resalt.max = num;
+    }
     if (num % 2 === 0) {
       num = num / 2;
     } else {
       num = num * 3 + 1;
     }
+    resalt.nass.push(num);
   }
-  return num;
+  resalt.steps = resalt.nass.length;
+  return resalt;
 }
-const x = 2;
-console.log(naesa(x));
+const arr = 9;
+const x = naesa(arr);
+console.log(x);
+console.log(x.steps);
